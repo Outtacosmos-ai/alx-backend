22252f33e5de81f3f5778fa2ad6d043c3e1662bb
Outtacosmos-ai <messghir4@gmail.com>
2024-11-07 13:59:50 +0100

    done ✅

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -6,23 +6,20 @@ from flask_babel import Babel, _
 app = Flask(__name__)
 babel = Babel(app)
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
 class Config:
     """Config class for Flask app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app.config.from_object(Config)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 @babel.localeselector
 def get_locale():
@@ -32,7 +29,6 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
 def get_user():
     """Returns a user dictionary or None if ID cannot be found"""
     user_id = request.args.get('login_as')
@@ -40,18 +36,15 @@ def get_user():
         return users.get(int(user_id))
     return None
 
-
 @app.before_request
 def before_request():
     """Find a user if any, and set it as a global on flask.g.user"""
     g.user = get_user()
 
-
 @app.route('/')
 def index():
     """Route for the index page"""
     return render_template('5-index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
