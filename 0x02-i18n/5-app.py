42d941d9123be19cc7f85b32f355504510a27e00
Outtacosmos-ai <messghir4@gmail.com>
2024-11-07 12:46:52 +0100

    done ✅

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 """Mock logging in"""
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, _
+from flask_babel import Babel
+from typing import Union, Dict
 
 app = Flask(__name__)
 babel = Babel(app)
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
 
 
 class Config:
@@ -22,36 +16,44 @@ class Config:
 
 
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
-@babel.localeselector
-def get_locale():
-    """Determine the best match with our supported languages"""
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-def get_user():
-    """Returns a user dictionary or None if ID cannot be found"""
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id."""
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
     return None
 
 
 @app.before_request
-def before_request():
-    """Find a user if any, and set it as a global on flask.g.user"""
+def before_request() -> None:
+    """Performs some routines before each request's resolution."""
     g.user = get_user()
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page."""
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route('/')
-def index():
+def index() -> str:
     """Route for the index page"""
     return render_template('5-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
