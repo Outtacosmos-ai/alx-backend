f4ae86d0f16a6d417198b18f3ef1c44c8cd3fd27
Outtacosmos-ai <messghir4@gmail.com>
2024-11-08 22:12:14 +0100

    done ✅

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 """
-Flask app with internationalization support and user locale preference
+Flask app with internationalization support and user login simulation
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
 
 
 class Config:
@@ -25,15 +18,25 @@ class Config:
 
 app.config.from_object(Config)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 
 def get_user():
     """
     Returns a user dictionary or None if ID cannot be found
     or if login_as was not passed
     """
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
+    try:
+        user_id = request.args.get('login_as')
+        if user_id:
+            return users.get(int(user_id))
+    except Exception:
+        return None
     return None
 
 
@@ -51,23 +54,16 @@ def get_locale():
     """
     Determine the best match with our supported languages
     """
-    # 1. Locale from URL parameters
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
-
-    # 2. Locale from user settings
-    if g.user and g.user['locale'] in app.config['LANGUAGES']:
-        return g.user['locale']
-
-    # 3. Locale from request header
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Render the index page"""
-    return render_template('6-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
