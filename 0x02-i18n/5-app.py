457ea57fb281e31778be48ed007545df500c717f
Outtacosmos-ai <messghir4@gmail.com>
2024-11-08 15:46:50 +0100

    done ✅

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -5,8 +5,10 @@ Flask app with internationalization support and mock user login
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -15,13 +17,16 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 class Config:
     """Configuration class for Flask app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 def get_user():
     """
@@ -33,6 +38,7 @@ def get_user():
         return users.get(int(user_id))
     return None
 
+
 @app.before_request
 def before_request():
     """
@@ -41,15 +47,18 @@ def before_request():
     """
     g.user = get_user()
 
+
 @babel.localeselector
 def get_locale():
     """Determine the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Render the index page"""
     return render_template('5-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
